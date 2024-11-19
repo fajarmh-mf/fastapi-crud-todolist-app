1ffdaff810a02609e0a8dd846fa37a19680d76bd
fajarmh-mf <hamka.fajar@moneyforward.co.jp>
2024-11-19 18:18:31 +0900

    feat: merge all items into main.py

--- src/main.py
+++ src/main.py
@@ -1,38 +1,22 @@
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
-from email_validator import validate_email, EmailNotValidError
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlmodel import select
+from .config.db import get_session, create_db_and_tables
+from .models.user import User
+from .controllers.user_controller import router as user_router
 import uvicorn
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
+app.include_router(user_router)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return { "item_id": item_id, "q": q }
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return { "item_name": item.name, "item_id": item_id } 
-
-@app.post("/check")
-def check_email(email: str):
-    try:
-        email_info = validate_email(email, check_deliverability=False)
-        email = email_info.normalized
-        return { "email": email }
-    except EmailNotValidError as e:
-        return { "error": str(e) }
-    
 
 def run():
     uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
