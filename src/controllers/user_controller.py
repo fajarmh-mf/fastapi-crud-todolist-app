3e6bb7eeb7b9789798fb7ba5736c56bb5008adf8
fajarmh-mf <hamka.fajar@moneyforward.co.jp>
2024-11-19 18:18:18 +0900

    feat: create a controller

---
+++ src/controllers/user_controller.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlmodel import select
+from ..config.db import get_session
+from ..models.user import User
+
+router = APIRouter()
+
+@router.post("/users/", response_model=User)
+def create_user(user: User, session: Session = Depends(get_session)):
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+@router.get("/users/{user_id}", response_model=User)
+def read_user(user_id: int, session: Session = Depends(get_session)):
+    user = session.get(User, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+@router.get("/users/", response_model=list[User])
+def read_users(session: Session = Depends(get_session)):
+    users = session.exec(select(User)).all()
+    return users
